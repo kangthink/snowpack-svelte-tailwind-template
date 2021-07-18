f15d3a0220245431700952ab97cff941212c559c
Alex <kangthink@gmail.com>
2021-07-18 16:39:20 +0900

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import './main.css'
+  import {onMount} from 'svelte';
+  import Hello from './components/Hello.svelte'
+
+  let count = 0;
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div class="app">
+  <div class="px-5 bg-green-300 box">
+    <div>TS Timer {count}</div>
+  </div>
+</div>
